fbf5cf80ef056031ab7b0f3bbd2f29ff504c1bd0
Dan Vanderkam <danvdk@gmail.com>
2007-10-11 06:41:10 +0000

    My perf test is getting way too fast!

--- perf_test.cc
+++ perf_test.cc
@@ -1,6 +1,7 @@
 // Evaluates 21,632 boards (mostly slight variations on a good and bad board)
 // and prints out information on the performance. Typical performance on my
 // machine is ~20kbds/sec, so this test takes just over one second to complete.
+const int reps = 10;
 
 #include <stdio.h>
 #include <sys/time.h>
@@ -21,40 +22,44 @@ int main(int argc, char** argv) {
   printf("Loaded %d words from dictionary\n", t.Size());
 
   Boggler b(&t);
-  unsigned int hash = 1234;
   unsigned int prime = (1 << 20) - 3;
   unsigned int total_score = 0;
+  unsigned int hash;
 
   char* bases[] = { "abcdefghijklmnop", "catdlinemaropets" };
+  int bds = sizeof(bases) / sizeof(*bases);
   double start = secs();
-  for (unsigned int i=0; i<sizeof(bases)/sizeof(*bases); ++i) {
-    b.ParseBoard(bases[i]);
-    for (int x1 = 0; x1 < 4; x1++) {
-      for (int x2 = 0; x2 < 4; x2++) {
-        for (int c1 = 0; c1 < 26; c1++) {
-          b.SetCell(x1, 1, c1);
-          for (int c2 = 0; c2 < 26; c2++) {
-            b.SetCell(x2, 2, c2);
-            int score = b.Score();
-            hash *= (123 + score);
-            hash = hash % prime;
-            total_score += score;
-          }
-        }
+  for (int rep = 0; rep < reps; rep++) {
+    hash = 1234;
+    for (int i=0; i<bds; ++i) {
+      b.ParseBoard(bases[i]);
+      for (int x1 = 0; x1 < 4; x1++) {
+	for (int x2 = 0; x2 < 4; x2++) {
+	  for (int c1 = 0; c1 < 26; c1++) {
+	    b.SetCell(x1, 1, c1);
+	    for (int c2 = 0; c2 < 26; c2++) {
+	      b.SetCell(x2, 2, c2);
+	      int score = b.Score();
+	      hash *= (123 + score);
+	      hash = hash % prime;
+	      total_score += score;
+	    }
+	  }
+	}
       }
     }
+    if (hash != 0x000C1D3D) {
+      fprintf(stderr, "Hash mismatch, expected 0xC1D3D\n");
+      return 1;
+    }
   }
+
   double end = secs();
   printf("Total score: %u = %lf pts/bd\n",
       total_score, 1.0 * total_score / b.NumBoards());
   printf("Score hash: 0x%08X\n", hash);
   printf("Evaluated %d boards in %lf seconds = %lf bds/sec\n",
       b.NumBoards(), (end-start), b.NumBoards()/(end-start));
-
-  if (hash != 0x000C1D3D) {
-    fprintf(stderr, "Hash mismatch, expected 0xC1D3D\n");
-    return 1;
-  }
   return 0;
 }
 

63810e413d2e3c278908a690b71da3ce8aeb8831
Dan Vanderkam <danvdk@gmail.com>
2008-06-29 02:54:00 +0000

    new ibucket_boggle, ibucket_breaker

--- ibucket_boggle.cc
+++ ibucket_boggle.cc
@@ -4,114 +4,11 @@
 // Play bucketed boggle w/o a bucketed trie. This could potentially be really
 // slow, but will give better bounds and allow more flexible bucketing.
 
-#include <math.h>
 #include <sys/time.h>
 #include "trie.h"
 #include "boggler.h"
-#include <vector>
-#include <map>
-#include <algorithm>
+#include "ibuckets.h"
 using namespace std;
-
-SimpleTrie* dict_ = NULL;
-int used_ = 0;
-char* bd_[16];
-unsigned int counts_[16];
-unsigned int choices_[16][26];
-
-map<SimpleTrie*, int> found_;  // word node -> length
-unsigned int cutoff = UINT_MAX;
-bool count_letters = true;
-unsigned int alt_score;
-unsigned int runs_ = 0;
-
-unsigned int DoDFS(int i, int len, SimpleTrie* t);
-unsigned int Score(SimpleTrie* t) {
-  unsigned int score = 0;
-  alt_score = 0;
-  memset(counts_, 0, sizeof(counts_));
-  memset(choices_, 0, sizeof(choices_));
-  used_ = 0;
-  dict_ = t;
-  runs_ += 1;
-  int i;
-  for (i=0; i<16; i++) {
-    unsigned int max_score = 0;
-    int choice = -1;
-    for (int j=0; bd_[i][j]; j++) {
-      int c =  bd_[i][j] - 'a';
-      if (t->StartsWord(c)) {
-        unsigned int tscore = DoDFS(i, 0, t->Descend(c));
-        if (tscore > max_score) {
-          max_score = tscore;
-          choice = c;
-        }
-      }
-    }
-    score += max_score;
-    if (choice >= 0)
-      choices_[i][choice] += 1;
-    if (score > cutoff && alt_score > cutoff)
-      return score;
-  }
-  printf("max, no mark score: %u\n", score);
-
-  // Calculate an alternate score
-  printf("superset of words score: %d\n", alt_score);
-
-  return min(alt_score, score);
-}
-
-unsigned int DoDFS(int i, int len, SimpleTrie* t) {
-  unsigned int score = 0;
-  used_ ^= (1 << i);
-  //len += (c==kQ ? 2 : 1);
-  len += 1;
-  if (t->IsWord()) {
-    // Should mark that this word has been found, but that's.. tricky
-    int word_score = BogglerBase::kWordScores[len];
-    score += word_score;
-    if (t->Mark() != runs_) {
-      alt_score += word_score;
-      t->Mark(runs_);
-    }
-    if (count_letters) {
-      for (int i=0; i<16; i++)
-        if (used_ & (1<<i)) counts_[i] += BogglerBase::kWordScores[len];
-    }
-  }
-
-  int cc, idx, choice;
-  int x = i / 4, y = i % 4;
-  unsigned int max_score, tscore;
-  for (int dx = -1; dx <= 1; dx++) {
-    if (x + dx < 0 || x + dx > 3) continue;
-    for (int dy = -1; dy <= 1; dy++) {
-      if (y + dy < 0 || y + dy > 3) continue;
-      idx = (x+dx) * 4 + y + dy;
-      if ((used_ & (1 << idx)) == 0) {
-        max_score = 0;
-        choice = -1;
-        for (int j=0; bd_[idx][j]; j++) {
-          cc = bd_[idx][j] - 'a';
-          if (t->StartsWord(cc)) {
-            tscore = DoDFS(idx, len, t->Descend(cc));
-            if (tscore > max_score) {
-              choice = cc;
-              max_score = tscore;
-            }
-          }
-        }
-        score += max_score;
-        if (choice != -1)
-          choices_[idx][choice] += 1;
-        if (score > cutoff && alt_score > cutoff) return score;
-      }
-    }
-  }
-  used_ ^= (1 << i);
-  return score;
-}
 
 double secs() {
   struct timeval t;
@@ -130,30 +27,39 @@ int main(int argc, char** argv) {
   const char* dict_file = argv[1];
   printf("loading words from %s\n", dict_file);
   SimpleTrie* t = GenericBoggler<SimpleTrie>::DictionaryFromFile(dict_file);
+  BucketBoggler bb(t);
 
+  char buf[400] = "";
   for (int i=2; i<argc; i++) {
-    bd_[i-2] = argv[i];
+    //strcpy(bb.Cell(i-2), argv[i]);
+    strcat(buf, argv[i]);
+    if (i < argc-1) strcat(buf, " ");
   }
+  if (!bb.ParseBoard(buf)) {
+    fprintf(stderr, "Couldn't parse %s\n", buf);
+    exit(1);
+  }
+  printf("Board: %s\n", bb.as_string());
 
   double start = secs();
-  int score = Score(t);
+  int score = bb.UpperBound();
   double end = secs();
-
-  printf("Cell usage:\n");
-  for (int i=0; i<16; i++) {
-    printf("%7u%c", counts_[i], i%4==3 ? '\n' : ' ');
-  }
-
-  printf("Frequency of letter choices:\n  ");
-  for (int i=0; i<16; i++)
-    printf("   (%d,%d)", i/4, i%4);
-  for (int j=0; j<26; j++) {
-    printf("\n%c:", 'a' + j);
-    for (int i=0; i<16; i++)
-      printf("%8d", choices_[i][j]);
-  }
-  printf("\n");
-
   printf("Score: %u\n", score);
   printf("%f secs elapsed\n", end - start);
+
+  const BucketBoggler::ScoreDetails& d = bb.Details();
+  printf("Details:\n");
+  printf(" num_reps: %llu = %fB\n", bb.NumReps(), bb.NumReps() / 1.0e9);
+  printf(" sum_union: %d\n", d.sum_union);
+  printf(" max_nomark: %d\n", d.max_nomark);
+
+  int cell = d.most_constrained_cell;
+  printf(" max+one: %d (force cell %d)\n",
+         d.max_nomark - d.one_level_win, cell);
+  if (cell >= 0) {
+    char* c = bb.Cell(cell);
+    for (int i=0; c[i]; i++) {
+      printf("   %c: %d\n", c[i], d.max_nomark - d.max_delta[cell][c[i] - 'a']);
+    }
+  }
 }
--- ibucket_breaker.cc
+++ ibucket_breaker.cc
@@ -7,10 +7,12 @@
 #include <algorithm>
 #include <math.h>
 #include <sys/time.h>
+#include <string>
 #include "trie.h"
 #include "boggler.h"
 #include "ibuckets.h"
-const int NumBoards = 10;
+const int NumBoards = 2;  //00;
+using std::string;
 
 double secs() {
   struct timeval t;
@@ -20,41 +22,121 @@ double secs() {
 
 // Generates a random bucketed board
 // aeiou sy bdfgjkmpvwxz chlnrt
+// aeio su fjkmvwxz dlnrt bcghpy
 void RandomBoard(char* buf) {
-  int used[26];
-  for (int i=0; i<16; i++) {
-    memset(used, 0, sizeof(used));
-    for (int j=0; j<4; j++) {
-      int c;
-      do { c = random() % 26; } while (used[c]);
-      *buf++ = 'a' + c;
-      used[c] = 1;
-    }
-    if (i<15) *buf++ = ' ';
-  }
+  //static char* classes[] = {"aeiou", "sy", "bdfgjkmpvwxz", "chlnrt"};
+  //static char* classes[] = {"aeio", "su", "fjkmvwxz", "dlnrt", "bcghpy"};
+  //static char* classes[] = {"aeiou", "chsy", "dlnrtbfgjkmpvwxz" };
+  static char* classes[] = {"abcdef", "ghijkl", "mnoprs", "tuvwxyz"};
+
+  static int num_classes = sizeof(classes)/sizeof(*classes);
+
   *buf = '\0';
+  for (int i=0; i<16; i++) {
+    strcat(buf, classes[random() % num_classes]);
+    if (i < 15) strcat(buf, " ");
+  }
 }
 
+// Use the board solving details to pick a bucket to kill and guess how many
+// board representatives this will kill.
+double DecreaseProb(int now, int then);
+int PickABucket(BucketBoggler& bb, double* expected_kills) {
+  int pick = -1;
+  *expected_kills = 1.0;
+  const BucketBoggler::ScoreDetails& d = bb.Details();
+  int base_score = d.max_nomark;
+  uint64_t reps = bb.NumReps();
+  for (int i=0; i<16; i++) {
+    double expectation = 0.0;
+    int choices = strlen(bb.Cell(i));
+    for (int j = 0; j < choices; j++) {
+      int reduced_score = base_score - d.max_delta[i][bb.Cell(i)[j] - 'a'];
+      expectation += (reps / choices) * DecreaseProb(reduced_score, 3625);
+    }
+    //printf("%2d: expect to kill %lf\n", i, expectation);
+    if (expectation > *expected_kills) {
+      *expected_kills = expectation;
+      pick = i;
+    }
+  }
+
+  if (pick == -1) {
+    pick = bb.Details().most_constrained_cell;
+  }
+
+  //for (int j = 0; bb.Cell(pick)[j]; j++) {
+  //  int reduced_score = base_score - d.max_delta[pick][bb.Cell(pick)[j] - 'a'];
+  //  printf("%c => %d @ %f\n", bb.Cell(pick)[j], reduced_score,
+  //                            DecreaseProb(reduced_score, 3625));
+  //}
+  return pick;
+}
+double DecreaseProb(int now, int then) {
+  static const double avg_decrease = 2.16;
+  static const double stddev_sqrt2 = 0.843 * sqrt(2.0);
+  if (now < then) return 1.0;
+  return 0.5 * (1.0 - erf((1.0 * now / then - avg_decrease) / stddev_sqrt2));
+}
+
+uint64_t orig_reps = 0;
 uint64_t elim = 0;
-bool ShedToConvergence(BucketBoggler& bb) {
+int max_depth = 0;
+bool ShedToConvergence(BucketBoggler& bb, int level) { 
+  if (level > max_depth) max_depth = level;
   int shed_letters=1;
   int bound;
   do {
     bound = bb.UpperBound();
     uint64_t reps = bb.NumReps();
 
-    printf("  => %llu reps, bound=%d (%d)",
+    printf("%s  => %llu reps, bound=%d (%d)", string(level, ' ').c_str(),
            reps, bound, bb.Details().max_nomark);
     if (bound >= 3625) {
       shed_letters = bb.ShedLetters(3625);
       uint64_t shed_reps = bb.NumReps();
       printf(", shed %d=%f: %s\n",
              shed_letters, 1.0*(reps-shed_reps)/reps, bb.as_string());
+      elim += (reps - shed_reps);
     } else {
+      elim += reps;
       printf(", DONE\n");
     }
   } while (bound >= 3625 && shed_letters > 0);
   return (bound < 3625);
+}
+
+void AttackBoard(BucketBoggler&, int level = 0, int num=1, int outof=1);
+void SplitBucket(BucketBoggler& bb, int cell, int level) {
+  char orig_bd[27 * 16];
+  char orig_cell[27];
+  strcpy(orig_bd, bb.as_string());
+  strcpy(orig_cell, bb.Cell(cell));
+
+  printf("%sWill evaluate %d more boards...\n",
+         string(level, ' ').c_str(), strlen(orig_cell));
+
+  for (int i=0; orig_cell[i]; i++) {
+    assert(bb.ParseBoard(orig_bd));
+    sprintf(bb.Cell(cell), "%c", orig_cell[i]);
+    AttackBoard(bb, level + 1, 1+i, strlen(orig_cell));
+  }
+}
+
+// Shed/Split until finished
+void AttackBoard(BucketBoggler& bb, int level, int num, int outof) {
+  uint64_t reps = bb.NumReps();
+  printf("(%2.2f%%)%s (%d;%d/%d) %s (%llu)\n", 100.0*elim/orig_reps,
+         string(level, ' ').c_str(), level, num, outof, bb.as_string(), reps);
+  fflush(stdout);
+  if (ShedToConvergence(bb, level)) {
+    return;
+  } else {
+    double expect;
+    int pick = PickABucket(bb, &expect);
+    SplitBucket(bb, pick, level);
+    //printf("expect to kill %lf boards by picking %d\n", expect, pick);
+  }
 }
 
 int main(int argc, char** argv) {
@@ -63,22 +145,45 @@ int main(int argc, char** argv) {
   SimpleTrie* t = GenericBoggler<SimpleTrie>::DictionaryFromFile("words");
   BucketBoggler bb(t);
 
-  double start = secs();
-  for (int i=0; i < NumBoards; i++) {
-    char bd[16 * 26];
-    RandomBoard(bd);
-    assert(bb.ParseBoard(bd));
-    printf("%3d: %s\n", i, bb.as_string());
-    uint64_t reps = bb.NumReps();
-    if (ShedToConvergence(bb)) {
-      elim += reps;
-    } else {
-      elim += (reps - bb.NumReps());
+  if (argc == 3) {
+    srandom(atoi(argv[2]));
+  } else if (argc == 2) {
+    time_t t = time(NULL);
+    srandom(t);
+    printf("Using seed %d\n", t);
+  } else if (argc > 3) {
+    if (argc != 18) {
+      fprintf(stderr, "Need %d args, got %d\n", 18, argc);
+      exit(1);
     }
-    printf("\n");
+  }
+
+  double start = secs();
+  uint64_t total_elim = 0;
+  for (int bds=0; bds < NumBoards; bds++) {
+    char bd[16 * 26] = "";
+    if (argc == 18) {
+      for (int i=2; i<argc; i++) {
+        strcat(bd, argv[i]);
+        if (i < argc-1) strcat(bd, " ");
+      }
+    } else {
+      RandomBoard(bd);
+    }
+
+    assert(bb.ParseBoard(bd));
+    elim = max_depth = 0;
+    orig_reps = bb.NumReps();
+    double a = secs();
+      AttackBoard(bb);
+    double b = secs();
+    printf("%llu reps in %.2f s @ depth %d = %f bds/sec:\n  %s\n",
+            elim, b-a, max_depth, 1.0*elim/(b-a), bd);
+    total_elim += elim;
   }
   double end = secs();
-  printf("elapsed: %fs = %f evals/sec\n", end-start, 1.0*NumBoards/(end-start));
+
+  printf("elapsed: %fs = %f sec/break\n", end-start, 1.0*(end-start)/NumBoards);
   printf("eliminated %llu boards = %f B bds/sec equivalent\n",
-         elim, 1.0 * elim / (end-start) / 1e9);
+         total_elim, 1.0 * total_elim / (end-start) / 1e9);
 }
